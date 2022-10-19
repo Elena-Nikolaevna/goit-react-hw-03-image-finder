eb6bfb634a0e41326ad203d2f84f64fbae174a24
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-20 01:51:08 +0300

    Update Searchbar.jsx

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,73 +1,57 @@
-import React, { Component } from 'react';
-import Notiflix from 'notiflix';
+//import React, { Component } from 'react';
+import { Formik } from 'formik';
+/* import Notiflix from 'notiflix'; */
 import styles from './Searchbar.module.css';
 import PropTypes from 'prop-types';
 
-class Searchbar extends Component {
-  state = {
-    name: '',
-    page: 1,
+export const Searchbar = ({ onSubmit }) => {
+  const handleSubmit = (values, { resetForm }) => {
+    onSubmit(values.searchQuery);
+    resetForm();
   };
-
-  handleChange = e => {
-    const { value } = e.currentTarget;
-    this.setState({ name: value });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-
-    if (this.state.name.trim() === '') {
-      Notiflix.Notify.failure(
-        'You have to enter something first to search for images!'
-      );
-      return;
-    }
-
-    this.props.onSubmitHandler(this.state);
-
-    this.reset();
-  };
-
-  reset() {
-    this.setState({ name: '' });
-  }
-
-  render() {
-    return (
-      <header className={styles.Searchbar}>
-        <form className={styles.SearchForm} onSubmit={this.handleSubmit}>
-          <button type="submit" className={styles.SearchFormButton}>
-            <span className={styles.SearchFormButtonLabel}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                viewBox="0 0 20 20"
+  return (
+    <header className={styles.Searchbar}>
+      <Formik initialValues={{ searchQuery: '', }} onSubmit={handleSubmit}>
+        {({ isSubmitting }) => (
+          <>
+            <button
+              type="submit"
+              className={styles.SearchFormButton}
+              //disabled={isSubmitting}
+            >
+              <span
+                className={styles.SearchFormButtonLabel}
+                //aria-label="Search"
               >
-                <title>search</title>
-                <path d="M19 17l-5.15-5.15a7 7 0 1 0-2 2L17 19zM3.5 8A4.5 4.5 0 1 1 8 12.5 4.5 4.5 0 0 1 3.5 8z" />
-              </svg>
-            </span>
-          </button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  viewBox="0 0 20 20"
+                >
+                  <title>search</title>
+                  <path d="M19 17l-5.15-5.15a7 7 0 1 0-2 2L17 19zM3.5 8A4.5 4.5 0 1 1 8 12.5 4.5 4.5 0 0 1 3.5 8z" />
+                </svg>
+              </span>
+            </button>
 
-          <input
-            className={styles.SearchFormInput}
-            type="text"
-            onChange={this.handleChange}
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-}
-
-Searchbar.propTypes = {
-  onSubmit: PropTypes.func,
-  onSubmitHandler: PropTypes.func.isRequired,
+            <input
+              className={styles.SearchFormInput}
+              name="searchQuery"
+              type="text"
+              autoComplete="off"
+              autoFocus
+              placeholder="Search images and photos"
+              //value={this.state.name}
+              //onChange={this.handleChange}
+            />
+          </>
+        )}
+      </Formik>
+    </header>
+  );
 };
 
-export default Searchbar;
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
