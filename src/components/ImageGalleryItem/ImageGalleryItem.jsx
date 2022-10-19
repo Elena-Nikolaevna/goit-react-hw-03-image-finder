8de7f88c192d3d901494665eea466b39272bcf57
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-20 01:50:59 +0300

    Update ImageGalleryItem.jsx

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,33 +1,29 @@
 import PropTypes from 'prop-types';
+//import { Modal } from 'components/Modal/Modal';
+//import { Component } from 'react';
 import styles from './ImageGalleryItem.module.css';
 
-function ImageGalleryItem({ articles, onImage }) {
-  return (
-    <>
-      {articles.map(({ id, webformatURL, largeImageURL, tags }) => (
-        <li className={styles.ImageGalleryItem} key={id}>
-          <img
-            src={webformatURL}
-            alt="response from API"
-            className={styles.ImageGalleryItemImage}
-            onClick={() => onImage(largeImageURL, tags, id)}
-          />
-        </li>
-      ))}
-    </>
-  );
-}
 
-export default ImageGalleryItem;
+export const ImageGalleryItem = ({ imageUrl, alt, id, onClick }) => {
+  return (
+    <li className={styles.ImageGalleryItem}>
+      <img
+      className={styles.ImageGalleryItemImage}
+        src={imageUrl}
+        alt={alt}
+        id={id}
+        onClick={evt => {
+          onClick(evt.target.id);
+        }}
+      />
+    </li>
+  );
+};
 
 ImageGalleryItem.propTypes = {
-  articles: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      webformatURL: PropTypes.string.isRequired,
-      tags: PropTypes.string.isRequired,
-      largeImageURL: PropTypes.string.isRequired,
-    })
-  ),
-  onImage: PropTypes.func.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+  id: PropTypes.number.isRequired,
+  onClick: PropTypes.func.isRequired,
 };
+
