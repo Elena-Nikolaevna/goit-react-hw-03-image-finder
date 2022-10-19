610d2766e7848db8a5556603b57c8d642b4ab7ce
Elena-Nikolaevna <98268833+Elena-Nikolaevna@users.noreply.github.com>
2022-10-20 01:51:11 +0300

    Update ImagesAPI.jsx

--- src/services/ImagesAPI.jsx
+++ src/services/ImagesAPI.jsx
@@ -1,8 +1,13 @@
-export const BASE_URL = 'https://pixabay.com/api/',
-  API_KEY = '28801959-c80fb9d6110e7835101e93207',
-  SEARCH_PARAMS = new URLSearchParams({
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    per_page: 12,
-  });
+
+import axios from 'axios';
+const API_KEY = '28801959-c80fb9d6110e7835101e93207';
+
+export function getImages(query, pageNumber) {
+  return axios
+    .get(
+      `https://pixabay.com/api/?q=${query}&page=${pageNumber}&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
+    )
+    .then(response => {
+      return response.data;
+    });
+}
